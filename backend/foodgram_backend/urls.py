a625708bf2f1cbeddbbb5f8809db7e4252338fad
Evgeniy Volkov <volkovev94@yandex.ru>
2024-03-15 21:12:06 +0300

    minor fixes

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include('users.urls')),
     path('api/', include(('api.urls', 'api'), namespace='receipt')),
+    path("api/", include('users.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
